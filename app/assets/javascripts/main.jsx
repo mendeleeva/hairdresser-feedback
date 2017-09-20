06f3cdeee2a295c24e879cacdde5eb1f2a0fe1af
YuliaSavova <yuliasavova@EPMACSI0000.local>
2017-09-20 17:15:03 +0300

    commit

--- app/assets/javascripts/main.jsx
+++ app/assets/javascripts/main.jsx
@@ -1,3 +1,15 @@
+class Main extends React.Component {
+    render() {
+        return (
+        <h1>Hello from the component</h1>
+        );
+
+    }
+    
+}
+
+
+
 let documentReady = () => {
      React.render(
       <h1>HoHoHo</h1>,
